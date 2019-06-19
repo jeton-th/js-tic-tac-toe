926a710d4e110caf24a16d1051c8ec6dd3117619
Jeton Thaçi <jetonthaci@outlook.com>
2019-06-19 15:54:46 +0200

    intial setup of board

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
 function play(element) {
-    console.log(element.value)
+  console.log(element.value)
 }
+
+const gameBoard = (() => {
+  const board = [];
+
+  return {
+    board,
+  };
+})();
+
+const displayController = (() => {
+  // const board = [];
+
+  // return {
+  //   board,
+  // };
+})();
+
+
+console.log(gameBoard.board)
