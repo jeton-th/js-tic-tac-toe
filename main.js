df4402486c7180d495ecfcfbab6707fd9af00375
Zeha Irawan <jangkaringbumi@gmail.com>
2019-06-20 22:33:06 +0700

    dumb bot

--- main.js
+++ main.js
@@ -5,19 +5,43 @@ const Player = (name, symbol) => ({ name, symbol });
 const player1 = Player('Player 1', 'X');
 const player2 = Player('Player 2', 'O');
 
-const bot = ()
+const bot = (() => {
+  const play = () => {
+    let botPlay = [];
+
+    gameBoard.board.forEach((e, i) => {
+      if (e === 0) botPlay.push(i);
+    });
+    console.log(botPlay);
+    let markIndex = botPlay[Math.floor(Math.random() * botPlay.length)];
+    let botMark = document.getElementById(markIndex);
+    botMark.disabled = true;
+    console.log(markIndex);
+    botMark.value = player2.symbol;
+  };
+  return { play };
+})();
+
+const botButton = document.querySelector('.bot');
+const name2 = document.querySelector('.name2');
+let isBot = false;
+
+botButton.addEventListener('click', () => {
+  displayController.disablePlayer2();
+  isBot = true;
+});
 
 const gameBoard = (() => {
   let board = new Array(9).fill(0);
 
   const disable = () => {
-    boxes.forEach((box) => {
+    boxes.forEach(box => {
       box.disabled = true;
     });
   };
 
   const restart = () => {
-    boxes.forEach((box) => {
+    boxes.forEach(box => {
       box.disabled = false;
       box.value = '';
     });
@@ -26,10 +50,16 @@ const gameBoard = (() => {
 
   const turn = () => {
     const zero = board.filter(a => a === 0);
-    if (zero.length % 2 === 0) {
+    // return zero.length % 2 === 1 || isBot ? player1 : player2;
+
+    if (isBot) {
+      bot.play();
+      return player1;
+    } else if (zero.length % 2 === 1) {
+      return player1;
+    } else {
       return player2;
     }
-    return player1;
   };
 
   const isFull = () => {
@@ -49,11 +79,11 @@ const gameBoard = (() => {
       [1, 4, 7],
       [2, 5, 8],
       [2, 4, 6],
-      [0, 4, 8],
+      [0, 4, 8]
     ];
 
     let winner = false;
-    combos.forEach((combo) => {
+    combos.forEach(combo => {
       if (combo.every(e => board[e] === 'X')) winner = true;
       if (combo.every(e => board[e] === 'O')) winner = true;
     });
@@ -71,22 +101,28 @@ const gameBoard = (() => {
     isFull,
     checkWinner,
     disable,
-    restart,
+    restart
   };
 })();
 
 const displayController = (() => {
-  const setName = () => {
-    const name1 = document.querySelector('.name1');
-    player1.name = (name1.value) ? name1.value : player1.name;
-    name1.disabled = true;
-
+  const disablePlayer2 = () => {
     const name2 = document.querySelector('.name2');
-    player2.name = (name2.value) ? name2.value : player2.name;
+    name2.value = 'Bot';
     name2.disabled = true;
   };
 
-  const winner = (player) => {
+  const setName = () => {
+    const name1 = document.querySelector('.name1');
+    player1.name = name1.value ? name1.value : player1.name;
+    name1.disabled = true;
+
+    const name2 = document.querySelector('.name2');
+    player2.name = name2.value ? name2.value : player2.name;
+    name2.disabled = true;
+  };
+
+  const winner = player => {
     document.querySelector('.msg').innerHTML = `Game over. <br> Winner is ${
       player.name
     }.`;
@@ -99,8 +135,6 @@ const displayController = (() => {
   const changeName = () => {
     const name1 = document.querySelector('.name1');
     name1.disabled = false;
-
-    const name2 = document.querySelector('.name2');
     name2.disabled = false;
   };
 
@@ -114,13 +148,15 @@ const displayController = (() => {
     changeName,
     tie,
     clearMessage,
+    disablePlayer2
   };
 })();
 
-boxes.forEach((box) => {
+boxes.forEach(box => {
   box.addEventListener('click', () => {
-    const player = gameBoard.turn();
+    botButton.disabled = true;
     box.disabled = true;
+    const player = gameBoard.turn();
     box.value = player.symbol;
 
     displayController.setName();
@@ -137,6 +173,8 @@ boxes.forEach((box) => {
 
 const restart = document.querySelector('.restart');
 restart.addEventListener('click', () => {
+  botButton.disabled = false;
+  name2.value = '';
   gameBoard.restart();
   displayController.changeName();
   displayController.clearMessage();
