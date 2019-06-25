5bdc13869ab5768b051a6cd6de1e390ef133fb4d
Zeha Irawan <jangkaringbumi@gmail.com>
2019-06-25 16:08:57 +0700

    update readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# js-tic-tac-toe
+### Tic Tac Toe
+
+This is an implementation of TicTacToe game in javascript using OOP principles.
+
+### Contributors:
+* [Jeton Thaçii](https://github.com/jeton-th)
+* [Zeha Irawan](https://github.com/JangkarBumi)
--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 /* eslint no-param-reassign: ["error", { "props": false }] */
 const boxes = document.querySelectorAll('.box');
+const restartButton = document.querySelector('.restart');
 
-const Player = (name, symbol) => ({ name, symbol });
-const player1 = Player('Player 1', 'X');
-const player2 = Player('Player 2', 'O');
+const Player = (name, symbol) => {
+  const moves = [];
+  const play = (index, board) => {
+    board.addMark(index, this.symbol);
+    moves.push(parseInt(index, 10));
+  };
 
-const gameBoard = (() => {
-  let board = new Array(9).fill(0);
+  return {
+    name,
+    symbol,
+    moves,
+    play
+  };
+};
+let player1 = Player('Player 1', 'X');
+let player2 = Player('Player 2', 'O');
 
+const Board = () => {
+  const getBoard = new Array(9).fill(0);
   const combos = [
     [0, 1, 2],
     [3, 4, 5],
@@ -16,75 +29,45 @@ const gameBoard = (() => {
     [1, 4, 7],
     [2, 5, 8],
     [2, 4, 6],
-    [0, 4, 8],
+    [0, 4, 8]
   ];
 
-  const disable = () => {
-    boxes.forEach((box) => {
-      box.disabled = true;
-    });
+  const addMark = (index, symbol) => {
+    getBoard[index] = symbol;
   };
 
-  const restart = () => {
-    boxes.forEach((box) => {
-      box.disabled = false;
-      box.value = '';
-    });
-    board = new Array(9).fill(0);
-  };
-
-  const turn = () => {
-    const zero = board.filter(a => a === 0);
-    if (zero.length % 2 === 0) {
-      return player2;
-    }
-    return player1;
-  };
+  const checkWinner = playerMoves =>
+    combos.some(combo => combo.every(e => playerMoves.includes(e)));
 
   const isFull = () => {
-    const zero2 = board.filter(a => a === 0);
+    const zero2 = getBoard.filter(a => a === 0);
     if (zero2.length === 0) {
       return true;
     }
     return false;
   };
 
-  const checkWinner = () => {
-    let winner = false;
-    combos.forEach((combo) => {
-      if (combo.every(e => board[e] === 'X')) winner = true;
-      if (combo.every(e => board[e] === 'O')) winner = true;
-    });
-    return winner;
-  };
-
-  const addMark = (index, player) => {
-    board[index] = player === player1 ? player1.symbol : player2.symbol;
-  };
-
   return {
-    board,
-    turn,
+    getBoard,
+    combos,
     addMark,
-    isFull,
     checkWinner,
-    disable,
-    restart,
+    isFull
   };
-})();
+};
 
-const displayController = (() => {
+const display = (() => {
   const setName = () => {
     const name1 = document.querySelector('.name1');
-    player1.name = (name1.value) ? name1.value : player1.name;
+    player1.name = name1.value ? name1.value : player1.name;
     name1.disabled = true;
 
     const name2 = document.querySelector('.name2');
-    player2.name = (name2.value) ? name2.value : player2.name;
+    player2.name = name2.value ? name2.value : player2.name;
     name2.disabled = true;
   };
 
-  const winner = (player) => {
+  const winner = player => {
     document.querySelector('.msg').innerHTML = `Game over. <br> Winner is ${
       player.name
     }.`;
@@ -104,38 +87,57 @@ const displayController = (() => {
 
   const clearMessage = () => {
     document.querySelector('.msg').innerHTML = '';
+    boxes.forEach(box => {
+      box.disabled = false;
+      box.value = '';
+    });
+  };
+
+  const disable = () => {
+    boxes.forEach(box => {
+      box.disabled = true;
+    });
   };
 
   return {
     setName,
-    winner,
     changeName,
-    tie,
     clearMessage,
+    disable,
+    winner,
+    tie
   };
 })();
 
-boxes.forEach((box) => {
+let newBoard = Board();
+
+boxes.forEach(box => {
   box.addEventListener('click', () => {
-    const player = gameBoard.turn();
+    const player =
+      newBoard.getBoard.filter(a => a === 0).length % 2 === 0
+        ? player2
+        : player1;
+
     box.disabled = true;
     box.value = player.symbol;
 
-    displayController.setName();
-    gameBoard.addMark(box.id, player);
+    display.setName();
+    player.play(box.id, newBoard);
 
-    if (gameBoard.checkWinner()) {
-      gameBoard.disable();
-      displayController.winner(player);
-    } else if (gameBoard.isFull()) {
-      displayController.tie();
+    if (newBoard.checkWinner(player.moves)) {
+      display.disable();
+      display.winner(player);
+    } else if (newBoard.isFull()) {
+      display.tie();
     }
   });
 });
 
-const restart = document.querySelector('.restart');
-restart.addEventListener('click', () => {
-  gameBoard.restart();
-  displayController.changeName();
-  displayController.clearMessage();
+restartButton.addEventListener('click', () => {
+  newBoard = Board();
+  player1 = Player('Player 1', 'X');
+  player2 = Player('Player 2', 'O');
+
+  display.changeName();
+  display.clearMessage();
 });
